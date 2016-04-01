0efb0f0cad1b2067e6c422906ddb7728d090a3c3
jeetprksh <jeet1989a@gmail.com>
2016-04-02 01:09:18 +0530

    committing the first demo source

---
+++ ui-demo/src/main/webapp/app.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular
+    	.module('app', ['ngRoute', 'ui.bootstrap',])
+        .config(config)
+        .run(run);
+   
+    config.$inject = ['$routeProvider'];
+    
+    function config($routeProvider) {
+        $routeProvider
+        	.when('/home', {
+        		controller: 'HomeController',
+        		templateUrl: 'modules/home/home.html',
+        		controllerAs: 'vm'
+        	})
+            .otherwise({ redirectTo: '/home' });
+    }
+    
+    function run() {
+        // TODO
+    }
+
+})();
