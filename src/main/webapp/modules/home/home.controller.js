910d7f5727f734ad63d8512f81b30a7a8ac44fed
jeetprksh <jeet1989a@gmail.com>
2016-04-02 23:59:52 +0530

    adding removed project directories

---
+++ src/main/webapp/modules/home/home.controller.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('HomeController', HomeController);
+
+    HomeController.$inject = ['$scope'];
+    
+    function HomeController($scope) {
+        var vm = this;   
+        
+		// TODO
+    }
+
+})();
